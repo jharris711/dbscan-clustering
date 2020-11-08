6b0c3eba17926dcc2427f0892b83e26751b555dd
Josh Harris <49429928+jharris711@users.noreply.github.com>
2020-11-07 23:42:47 -0500

    intiial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+
+import reducer from './reducers'
+import App from './App'
+import './index.css'
+
+const middleware = [thunk]
+
+const store = createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(...middleware))
+)
+
+render(
+    <Provider store={store} >
+        {' '}
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
